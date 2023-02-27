5f55823095e0899edc9f0b41db6e825f6e343418
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 11:32:53 +0600

    Update AuthStack.js

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -2,11 +2,11 @@ import { Flash, Login, SignUp } from "../screens";
 import navigationStrings from "./navigationStrings";
 
 export default function (Stack) {
-  return (
+    return (
     <>
-      {/* <Stack.Screen name={navigationStrings.FLASH} component={Flash} /> */}
-      <Stack.Screen name={navigationStrings.LOGIN} component={Login} />
-      <Stack.Screen name={navigationStrings.SIGNUP} component={SignUp} />
+        {/* <Stack.Screen name={navigationStrings.FLASH} component={Flash} /> */}
+        <Stack.Screen name={navigationStrings.LOGIN} component={Login}  />
+        <Stack.Screen name={navigationStrings.SIGNUP} component={SignUp} />
     </>
-  );
+    )
 }
