ad2eafea541c386e6697f9e13f43df8ca5617fdd
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 10:50:29 +0600

    Update Porfile.js

--- src/screens/main/Porfile.js
+++ src/screens/main/Porfile.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import CustomButton from '../../components/CustomButton'
 
 const Porfile = () => {
   return (
     <View>
-      <Text>Porfile</Text>
+      <Text>Name: {userData.name}</Text>
+      <CustomButton title={'Logout'} handleNavigation={handleLogout} isLoading={loading} />
     </View>
   )
 }
