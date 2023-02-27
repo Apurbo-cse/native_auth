f848776f0bb47566f1aaa93c56e2211b83897245
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 10:22:23 +0600

    Update SignUpScreen.js

--- src/screens/auth/SignUpScreen.js
+++ src/screens/auth/SignUpScreen.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, SafeAreaView } from 'react-native'
+import React from "react";
+import TextInputField from '../../components/TextInputField';
+import CustomButton from '../../components/CustomButton';
 
 const SignUpScreen = () => {
   return (
-    <View>
-      <Text>SignUpScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <TextInputField
+          label={"User Name"}
+          placeholder="Enter your User Name"
+          onChangeText={(e) => setstate({ ...state, first_name: e })}
+        />
+        <TextInputField
+          label={"Email"}
+          placeholder="Enter your Email"
+          onChangeText={(e) => setstate({ ...state, email: e })}
+        />
+        <TextInputField
+          label={"Phone"}
+          placeholder="Enter your Phone no"
+          onChangeText={(e) => setstate({ ...state, phone: e })}
+        />
+        <TextInputField
+          label={"Password"}
+          placeholder="Enter your Password"
+          secureTextEntry={isSecure}
+          onChangeText={(e) => setstate({ ...state, password: e })}
+        />
 
-export default SignUpScreen
+        <CustomButtons
+          isLoading={isLoading}
+          title="Signup"
+          handleNavigation={handleRoute}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default SignUpScreen;
+
+const styles = StyleSheet.create({});
