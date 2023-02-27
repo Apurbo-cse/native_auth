564bc463cf8de4a54659014d870bd895dee7edb4
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 10:54:07 +0600

    Create TabRoute.js

---
+++ src/navigations/TabRoute.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function TabRoute() {
+  return (
+    <View>
+      <Text>TabRoute</Text>
+    </View>
+  )
+}
