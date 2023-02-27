a91331a287aa648c2dbd3da8a3ff90190e4797c8
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 11:37:28 +0600

    Update Porfile.js

--- src/screens/main/Porfile.js
+++ src/screens/main/Porfile.js
@@ -1,16 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import CustomButton from '../../components/CustomButton'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import CustomButton from "../../components/CustomButton";
+import actions from "../../redux/actions";
+import { useState } from "react";
 
-const Porfile = () => {
+const Profile = () => {
+  const userData = useSelector((state) => state.auth.userData);
+  const [loading, setLoading] = useState(false);
+  function handleLogout() {
+    setLoading(true);
+    setTimeout(() => {
+      actions.logout();
+      setLoading(false);
+    }, 2000);
+  }
   return (
     <View>
       <Text>Name: {userData.name}</Text>
-      <CustomButton title={'Logout'} handleNavigation={handleLogout} isLoading={loading} />
+      <CustomButton
+        title={"Logout"}
+        handleNavigation={handleLogout}
+        isLoading={loading}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Porfile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
