f568ab0244dfe9dc12ddc8095e8a1f5a00914594
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 10:15:51 +0600

    Update TextInputField.js

--- src/components/TextInputField.js
+++ src/components/TextInputField.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, TextInput, SafeAreaView } from 'react-native'
 import React from 'react'
 
 
