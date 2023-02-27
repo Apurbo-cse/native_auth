c3f0bac28bd91f724e663f7a8679e7954c78b6a9
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-27 10:53:08 +0600

    Create navigationString.js

---
+++ src/navigations/navigationString.js
@@ -0,0 +1,15 @@
+export default {
+    //tab
+    TABROUTE: "TABROUTE",
+    HOMESTACK: "HOMESTACK",
+  
+    // auth
+    FLASH: "FlashScreen",
+    LOGIN: "Login",
+    SIGNUP: "Signup",
+  
+    //main
+    HOME: "HOME",
+    PROFILE: "PROFILE",
+  };
+  
